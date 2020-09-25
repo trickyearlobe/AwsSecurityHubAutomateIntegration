c5186ece7503496a2a65031f6f287170d6fc32dc
Richard Nixon <richard.nixon@btinternet.com>
2020-09-25 04:08:10 +0100

    Process results in batches and update workflow status (pass/fail = new/resolved)

--- lambda_function.rb
+++ lambda_function.rb
@@ -57,65 +57,75 @@ class ComplianceReport
   end
 
   def process_profile(profile)
-    puts "Processing profile #{profile['name']}"
-    findings = []
-
-    profile['controls'].each do |control|
-
-      # Check if any results caused the overall control to fail
-      control_failed=false
-      control['results'].each do |result|
-        if result['status'] == "failed"
-          control_failed = true
-        end
-      end unless control['results'].nil?
+    return unless profile['controls'] # Dont try process an empty control set
+    puts "Processing profile #{profile['name']} with #{profile['controls'].length} controls"
+    while profile['controls'].length > 0
+      controls = profile['controls'].pop(100) # Respect AWS batch import limits
+      findings = controls.map{ |control| finding_from_control(profile['name'], control)}
+      success_updates = updates_from_findings(findings,"RESOLVED")
+      failed_updates = updates_from_findings(findings,"NEW")
       
-      # Handle long or missing control descriptions
-      control['desc'] = control['id'] unless control['desc']
-      control['desc'] = control['desc'][0...1000]+"...(truncated)" if control['desc'].length > 1024
-
-      # Handle missing impacts
-      control['impact'] = 1 unless control['impact']
-
-      # For now, send only failed controls as AWS has a limit of 100 findings per batch
-      # TODO: Make a findings queue to allow us to send batches of 100
-      if control_failed
-        finding = {
-          schema_version: "2018-10-08",
-          id: "#{profile['name']} #{control['id']}",
-          product_arn: "arn:aws:securityhub:#{ENV['AWS_REGION']}:#{aws_account_id}:product/#{aws_account_id}/default",
-          generator_id: "Inspec #{profile['name']}",
-          aws_account_id: "#{aws_account_id}",
-          types: ["Software and Configuration Checks/Industry and Regulatory Standards/CIS Host Hardening Benchmarks"],
-          last_observed_at: report_timestamp,
-          created_at: report_timestamp,
-          updated_at: report_timestamp,
-          severity: {
-            normalized: control['impact'] * 100
-          },
-          title: "#{profile['name']} #{control['id']}",
-          description: control['desc'],
-          source_url: "#{automate_server}/compliance/reports/nodes/#{node_id}",
-          resources: [
-            type: "Other",
-            id: node_name,
-            partition: "aws",
-            region: ENV['AWS_REGION'],
-          ],
-          compliance: {
-            status: control_failed ? "FAILED" : "PASSED"
-          },
-          workflow: {
-            status: "NEW"
-          },
-        }
-        findings << finding
-      end
-    end unless profile['controls'].nil?
-    if findings.length > 0
-      puts "Sending #{findings.length} results"
-      puts @shclient.batch_import_findings(findings: findings)
+      puts "BatchImportFindings: #{@shclient.batch_import_findings(findings: findings)}"
+      puts "RESOLVED BatchUpdateFindings: #{@shclient.batch_update_findings(success_updates)}" if success_updates[:finding_identifiers].length > 0
+      puts "NEW      BatchUpdateFindings: #{@shclient.batch_update_findings(failed_updates)}" if failed_updates[:finding_identifiers].length > 0
     end
+  end
+
+  def finding_from_control(profile, control)
+    # Handle long or missing control descriptions/impacts
+    control['desc'] = control['id'] unless control['desc']
+    control['desc'] = control['desc'][0...1000]+"...(truncated)" if control['desc'].length > 1024
+    control['impact'] = 1 unless control['impact']
+    # Construct an ASFF structure for AWS Security Hub
+    finding = {
+      schema_version: "2018-10-08",
+      id: "#{profile} #{control['id']} #{node_id}",
+      product_arn: "arn:aws:securityhub:#{ENV['AWS_REGION']}:#{aws_account_id}:product/#{aws_account_id}/default",
+      generator_id: "Inspec #{profile}",
+      aws_account_id: "#{aws_account_id}",
+      types: ["Software and Configuration Checks/Industry and Regulatory Standards/CIS Host Hardening Benchmarks"],
+      last_observed_at: report_timestamp,
+      created_at: report_timestamp,
+      updated_at: report_timestamp,
+      severity: {
+        normalized: control['impact'] * 100
+      },
+      title: "#{profile} #{control['id']}",
+      description: control['desc'],
+      source_url: "#{automate_server}/compliance/reports/nodes/#{node_id}",
+      resources: [
+        type: "Other",
+        id: node_name,
+        partition: "aws",
+        region: ENV['AWS_REGION'],
+      ],
+      compliance: {
+        status: control_status(control) # Control status is based on a result set
+      },
+      workflow: {
+        status: control_status(control) == "PASSED" ? "RESOLVED" : "NEW"
+      },
+    }
+  end
+
+  def control_status(control)
+    return "PASSED" unless control['results'] # Dont try process a control with no results
+    if control['results'].map{ |result| result['status'] }.include? "failed"
+      return "FAILED"
+    else
+      return "PASSED"
+    end
+  end
+
+  def updates_from_findings(findings, status)
+    {
+      finding_identifiers: findings.select{
+        |finding| finding[:workflow][:status]==status
+      }.map{
+        |finding| {id:finding[:id], product_arn:finding[:product_arn]}
+      },
+      workflow: { status: status }
+    }
   end
 end
 
